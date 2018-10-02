5bf79270702e6ca976f523bc98d53e66c4c0e964
Kacper Łukasik <sakuy124@gmail.com>
2018-10-02 17:38:54 +0200

    Update

--- contactmanager/src/components/contacts/AddContact.js
+++ contactmanager/src/components/contacts/AddContact.js
@@ -1,115 +1,106 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import { Consumer } from '../../context'
-import TextInputGroup from '../layout/TextInputGroup'
-import axios from 'axios'
+import React, { Component } from 'react';
+import TextInputGroup from '../layout/TextInputGroup';
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { addContact } from "../../actions/contactActions";
+import uuid from 'uuid'
+
 class AddContact extends Component {
-    state = {
-        name: '',
-        email: '',
-        phone: '',
-        errors : {},
+  state = {
+    name: '',
+    email: '',
+    phone: '',
+    errors: {}
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const { name, email, phone } = this.state;
+
+    // Check For Errors
+    if (name === '') {
+      this.setState({ errors: { name: 'Name is required' } });
+      return;
     }
-    onSubmit = async (dispatch, e) => {
-        e.preventDefault();
-        const { name, email, phone } = this.state;
-        if(name === ''){
-            this.setState({
-                errors : { name: 'Name is required'}
-            })
-            return;
-        }
-        if(email === ''){
-            this.setState({
-                errors : { email: 'Email is required'}
-            })
-            return;
-        }
-        if(phone === ''){
-            this.setState({
-                errors : { phone: 'Phone is required'}
-            })
-            return;
-        }
-        const newContact = {
-            name,
-            email,
-            phone,
 
-        }
-        const res = await axios.post(
-            'https://jsonplaceholder.typicode.com/users/',
-            newContact
-        );
-                dispatch({type: 'ADD_CONTACT', payload :
-                    res.data});
+    if (email === '') {
+      this.setState({ errors: { email: 'Email is required' } });
+      return;
+    }
 
-        this.setState({
-            name: '',
-            email: '',
-            phone: '',
-            errors : {}
-        })
+    if (phone === '') {
+      this.setState({ errors: { phone: 'Phone is required' } });
+      return;
+    }
 
-        this.props.history.push('/')
+    const newContact = {
+      name,
+      email,
+      phone
+    };
+
+    //// SUBMIT CONTACT ////
+    this.props.addContact(newContact)
+    // Clear State
+    this.setState({
+      name: '',
+      email: '',
+      phone: '',
+      errors: {}
+    });
+
+    this.props.history.push('/');
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, phone, errors } = this.state;
+
+    return (
+      <div className="card mb-3">
+        <div className="card-header">Add Contact</div>
+        <div className="card-body">
+          <form onSubmit={this.onSubmit}>
+            <TextInputGroup
+              label="Name"
+              name="name"
+              placeholder="Enter Name"
+              value={name}
+              onChange={this.onChange}
+              error={errors.name}
+            />
+            <TextInputGroup
+              label="Email"
+              name="email"
+              type="email"
+              placeholder="Enter Email"
+              value={email}
+              onChange={this.onChange}
+              error={errors.email}
+            />
+            <TextInputGroup
+              label="Phone"
+              name="phone"
+              placeholder="Enter Phone"
+              value={phone}
+              onChange={this.onChange}
+              error={errors.phone}
+            />
+            <input
+              type="submit"
+              value="Add Contact"
+              className="btn btn-light btn-block"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
-onChange = e => this.setState({[e.target.name]: e.target.value})
-    render() {
-        const {name,email,phone, errors} = this.state;
-
-
-
-        return(
-            <Consumer>
-                {value => {
-                    const { dispatch } = value;
-                    return (
-                        <div className="card mb-3">
-                            <div className="card-header">
-                                Add Contact
-                            </div>
-                            <div className="card-body">
-                                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
-                                    <TextInputGroup
-                                        label="Name"
-                                        name="name"
-                                        placeholder="Enter Name"
-                                        value={name}
-                                        onChange={this.onChange}
-                                        error={errors.name}
-
-                                    />
-                                    <TextInputGroup
-                                        label="Email"
-                                        name="email"
-                                        type='email'
-                                        placeholder="Enter Email"
-                                        value={email}
-                                        onChange={this.onChange}
-                                        error={errors.email}/>
-                                    <TextInputGroup
-                                        label="Phone"
-                                        name="phone"
-                                        placeholder="Enter Phone"
-                                        value={phone}
-                                        onChange={this.onChange}
-                                        error={errors.phone}/>
-
-                                    <input type="submit" value="Add Contact"
-                                           className="btn btn-light btn-block"/>
-                                </form>
-                            </div>
-                        </div>
-                    )
-                }}
-            </Consumer>
-        )
-
-
-
-    }
+AddContact.propTypes = {
+  addContact: PropTypes.func.isRequired,
 }
 
-
-
-export default AddContact;
+export default connect(null, { addContact })(AddContact);
--- contactmanager/src/components/contacts/Contact.js
+++ contactmanager/src/components/contacts/Contact.js
@@ -1,78 +1,66 @@
-import React, {Component} from 'react';
-import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
-import axios from 'axios'
-import { Consumer } from '../../context'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux'
+import {deleteContact} from "../../actions/contactActions";
+
 class Contact extends Component {
-    state = {
-        showContactInfo : false,
-    }
-    onDeleteClick = async (id, dispatch) =>{
-        try {
-            await axios.delete
-            (`https://jsonplaceholder.typicode.com/users/${id}`);
-            dispatch({type: 'DELETE_CONTACT', payload: id})
-        } catch (e) {
-            dispatch({type: 'DELETE_CONTACT', payload: id})
-        }
+  state = {
+    showContactInfo: false
+  };
 
+  onDeleteClick = id => {
+   this.props.deleteContact(id)
+  };
 
+  render() {
+    const { id, name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
 
-
-    }
-
-
-
-
-    render() {
-        const { id, name,email,phone } = this.props.contact;
-        const { showContactInfo } = this.state;
-        return (
-            <Consumer>
-                {value => {
-                    const { dispatch } = value;
-                    return (
-                        <div className="card card-body mb-3">
-                            <h4>{name} <i
-                                onClick={()=> this.setState({
-                                    showContactInfo: !this.state.showContactInfo
-                                })} className="fas fa-sort-down"
-                                style={{cursor: 'pointer',}}></i>
-                                <div className="fas fa-times" style={{cursor: 'pointer', float: 'right', color: 'red'}}
-                                     onClick={this.onDeleteClick.bind(this, id, dispatch)}
-                                ></div>
-                                <Link to={`contact/edit/${id}`}>
-                                    <i
-                                        className="fas fa-pencil-alt"
-                                        style={{
-                                            cursor: 'pointer',
-                                            float: 'right',
-                                            color: 'black',
-                                            marginRight: '1rem',
-                                        }}
-                                    ></i>
-                                </Link>
-
-
-                            </h4>
-                            {showContactInfo ? ( <ul className='list-group'>
-                                <li className='list-group-item'>Email: {email}</li>
-                                <li className='list-group-item'>Phone: {phone}</li>
-                            </ul>) : null }
-
-                        </div>
-                    )
-                }}
-            </Consumer>
-
-
-
-        );
-    }
+    return (
+      <div className="card card-body mb-3">
+        <h4>
+          {name}{' '}
+          <i
+            onClick={() =>
+              this.setState({
+                showContactInfo: !this.state.showContactInfo
+              })
+            }
+            className="fas fa-sort-down"
+            style={{ cursor: 'pointer' }}
+          />
+          <i
+            className="fas fa-times"
+            style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+            onClick={this.onDeleteClick.bind(this, id)}
+          />
+          <Link to={`contact/edit/${id}`}>
+            <i
+              className="fas fa-pencil-alt"
+              style={{
+                cursor: 'pointer',
+                float: 'right',
+                color: 'black',
+                marginRight: '1rem'
+              }}
+            />
+          </Link>
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
+      </div>
+    );
+  }
 }
+
 Contact.propTypes = {
-    contact : PropTypes.object.isRequired,
+  contact: PropTypes.object.isRequired,
+    deleteContact: PropTypes.func.isRequired,
 };
 
-
-export default Contact;
+export default connect(null, {deleteContact}) (Contact);
